9f3fc5f0e4eb395742c4b6742a5a539bb9036020
Tetiank-a <58983291+Tetiank-a@users.noreply.github.com>
2020-05-27 15:00:58 +0300

    version27.05(3pm)

---
+++ code/Code/Code/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    [Serializable]
+    public class Country
+    {
+        public string Name { get; set; }
+        public int key { get; set; }
+        public Country(string name, int k)
+        {
+            Name = name;
+            key = k;
+        }
+    }
+}
